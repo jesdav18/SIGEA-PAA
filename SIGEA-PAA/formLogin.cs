b33ef4f30846cf843043fcb1e07a1cb2b549ec10
jesdav <jesdav18@gmail.com>
2018-03-29 01:05:04 -0600

    Se agregó formulario de Login

---
+++ SIGEA-PAA/formLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+
+namespace SIGEA_PAA
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void formLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
